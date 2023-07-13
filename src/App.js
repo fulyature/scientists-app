f93566641ea40477065b867f3cc5b6f4a0cd8929
fulya_ture <fulyature@gmail.com>
2023-07-13 16:57:10 +0300

    useState,dataFiltered,Event

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import Header from "./components/Header";
 import { Container } from "react-bootstrap";
+
+import Header from "./components/Header";
+import ContainerCard from "./components/ContainerCard";
 
 const App = () => {
   return (
     <div>
       <Container className="text-center mt-5">
         <Header />
+        <ContainerCard />
       </Container>
     </div>
   );
--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Form from "react-bootstrap/Form";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useState } from "react";
+import { data } from "../helpers/data";
 
 const ContainerCard = () => {
-  return <div>ContainerCard</div>;
+  const [search, setSearch] = useState("");
+  const handleClick = (e) => {
+    setSearch(e.target.value);
+  };
+
+  const filteredData = data.filter((item) =>
+    item.name.toLowerCase().includes(search.trim().toLowerCase())
+  );
+  console.log(filteredData);
+  return (
+    <>
+      <Form.Control
+        type="search"
+        placeholder="Search Player..."
+        onChange={handleClick}
+      />
+      <Container className="p-3 rounded-3 card-container my-3">
+        <Row
+          xs={1}
+          sm={2}
+          md={3}
+          lg={4}
+          className="justify-content-center g-3 "
+        >
+          <Col>1</Col>
+          <Col>2</Col>
+          <Col>3</Col>
+        </Row>
+      </Container>
+    </>
+  );
 };
 
 export default ContainerCard;
